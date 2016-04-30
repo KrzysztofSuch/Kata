33981013a0aa0223cb15603a2894ffe7265b92a6
KrzysztofSuch <krzysztof.suchomski@outlook.com>
2016-04-30 17:17:05 +0200

    Codewars - TripleDouble

---
+++ Codewars/Triple trouble/Kata.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Kata
+{
+    public static int TripleDouble(long num1, long num2)
+    {
+        var isThereTriple = false;
+
+        var num1String = num1.ToString();
+
+        for(int i = 1; i < num1String.Length-1; i++)
+        {
+            if (num1String[i - 1] == num1String[i] && num1String[i] == num1String[i+1])
+            {
+                if (isDoubleCharInString(num1String[i],num2.ToString()))
+                    return 1;
+            }
+        }
+
+
+        return 0;
+        //code me ^^
+    }
+
+    private static Boolean isDoubleCharInString(Char searcherChar, String targetedString)
+    {
+        for(int i = 1; i < targetedString.Length; i++)
+        {
+            if (targetedString[i] == searcherChar && targetedString[i] == targetedString[i - 1])
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ Codewars/TripleTroubleTests/KataTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripleTroubleTests
+{
+    [TestFixture]
+    class KataTests
+    {
+        [Test]
+        [TestCase(451999277, 41177722899)]
+        [TestCase(666789, 12345667)]
+        [TestCase(10560002, 100)]
+        public static void Expected_1(long s1, long s2)
+        {
+            var result = Kata.TripleDouble(s1, s2);
+            Assert.AreEqual(1, result);
+        }
+
+
+        [TestCase(1222345, 12345)]
+        [TestCase(12345, 12345)]
+        public static void Expected_0(long s1, long s2)
+        {
+            var result = Kata.TripleDouble(s1, s2);
+            Assert.AreEqual(0, result);
+        }
+
+    }
+}
