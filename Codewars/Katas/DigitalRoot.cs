2b863daa7dc8aacb96b0d91dc22829b147498035
KrzysztofSuch <krzysztof.suchomski@outlook.com>
2016-05-01 19:48:24 +0200

    Codewars_DigitalRoots

---
+++ Codewars/Katas/DigitalRoot.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas
+{
+    public class DigitalRoots
+    {
+        public int DigitalRoot(long n)
+        {
+
+            if (n < 10)
+                return (int)n;
+
+            //var s =        ;
+            var sum = n.ToString().ToArray().Sum(parser());
+
+            if (sum > 9)
+                sum = DigitalRoot(sum);
+
+            return sum;
+            // Your awesome code here!
+        }
+
+        private static Func<Char, Int32> parser()
+        {
+            return a => int.Parse(a.ToString());
+        }
+    }
+}
---
+++ Codewars/KatasTests/DigitalRootsTests.cs
@@ -0,0 +1,51 @@
+﻿using Katas;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KatasTests
+{
+    [TestFixture]
+    public class DigitalRootsTests
+    {
+        private DigitalRoots num;
+
+        [SetUp]
+        public void SetUp()
+        {
+            num = new DigitalRoots();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            num = null;
+        }
+
+        [Test]
+        public void DigitalRootsTests_BasicTest()
+        {
+            Assert.AreEqual(7, num.DigitalRoot(16));
+        }
+
+        [Test]
+        public void DigitalRootsTests_BasicTest2()
+        {
+            Assert.AreEqual(6, num.DigitalRoot(942));
+        }
+
+        [Test]
+        public void DigitalRootsTests_BasicTest3()
+        {
+            Assert.AreEqual(6, num.DigitalRoot(132189));
+            Assert.AreEqual(2, num.DigitalRoot(493193));
+            
+        }
+
+        
+
+    }
+}
